8086b42c1615cea0f851ec523d8827cad21bbed4
Tiago Morgado <Tiago.alcm@icloud.com>
2023-06-28 23:43:02 +0200

    updated link routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+background:red;
+color:white;
+font-size: 2rem;
+padding: 1rem;
+`
+
 
 const Navbar = () => {
   return (
     <nav>
         <div className="nav-center">
+            <StyledBtn>ClickMe</StyledBtn>
             <span className="logo">MixMaster</span>
             <div className="nav-links">
                 <NavLink to='/' className='nav-link'>
