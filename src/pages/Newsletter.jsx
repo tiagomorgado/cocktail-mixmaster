ba44d70d3f394e72b4365f3c82dcdd2f15c937df
Tiago Morgado <Tiago.alcm@icloud.com>
2023-07-01 23:50:07 +0200

    setup newsletter form

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -7,9 +7,15 @@ const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 export const action = async ({request}) => {
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
-  const response = await axios.post(newsletterUrl,data)
-  toast.success(response.data.msg)
-  return redirect('/')
+
+  try {
+    const response = await axios.post(newsletterUrl,data)
+    toast.success(response.data.msg)
+    return redirect('/')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
 }
 
 const Newsletter = () => {
